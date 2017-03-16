fc7cac75d8fb715d0fee0f40386f959a7d85b7a8
unknown <cuongnv1@cuongnv1.toshiba-tsdv.com>
2017-03-16 17:14:29 +0700

    add some samples

---
+++ sample2/models/user.go
@@ -0,0 +1,11 @@
+package models
+
+type (  
+    // User represents the structure of our resource
+    User struct {
+        Name   string `json:"name"`
+        Gender string `json:"gender"`
+        Age    int    `json:"age"`
+        Id     string `json:"id"`
+    }
+)
